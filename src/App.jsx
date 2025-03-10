cc203457e784a958be2b534b9764edd9e18ed1d5
SansKc <sanskarkc51@gmail.com>
2025-03-10 18:27:13 +0545

    Sakyo project

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,22 @@
 import "./App.css";
-import Navbar from './components/Navbar/Navbar.jsx'
-import Hero from './components/Hero/Hero.jsx'
+import Navbar from "./components/Navbar/Navbar.jsx";
+import Hero from "./components/Hero/Hero.jsx";
 import About from "./components/AboutMe/About.jsx";
 import Project from "./components/Project/Project.jsx";
 import Newsletter from "./components/Newsletter/Newsletter.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 
-
-
 function App() {
-
-  
-  return  (
+  return (
     <div>
-    
-   
-    <Navbar/>
-    <Hero/>
-    <About/>
-    <Newsletter/>
-    <Project/>
-    <Footer/>
+      <Navbar />
+      <Hero />
+      <About />
+      <Newsletter />
+      <Project />
+      <Footer />
     </div>
-  )
+  );
 }
 
 export default App;
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+
+
 
 const Footer = () => {
   return (
-    <div className="grid grid-cols-[500px] grid-rows-[repeat(2,500px)]  text-white">
-      <div>01</div>
-      <div>02</div>
-    </div>
+    <footer className="bg-black text-white py-6 relative">
+      <h1 className="absolute px-80 text-2xl text-[#00df9a] font-extrabold w-[100px]" >REACT.</h1>
+      <div className="container mx-auto text-center">
+        <div className="grid grid-rows font-semibold ">
+          <h1>Home</h1>
+          <h1>About us</h1>
+          <h1>My Projects</h1>
+        </div>
+        <p className="text-sm">&copy; {new Date().getFullYear()} Sanskar Kc. All rights reserved.</p>
+        <div className="flex justify-center space-x-4 mt-2">
+          <a href="https://github.com/SansKc" target="_blank" className="hover:text-[#00df9a]">GitHub</a>
+          <a href="https://www.linkedin.com/in/sanskar-k-c-777a682a9/" target="_blank" className="hover:text-[#00df9a]">LinkedIn</a>
+          <a href="https://mail.google.com/mail/u/0/#inbox" className="hover:text-[#00df9a]">Email</a>
+        </div>
+      </div>
+    </footer>
   );
 };
 
