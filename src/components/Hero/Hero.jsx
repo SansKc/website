7c680ff1b494d97710f952480b55c2f82580650c
SansKc <sanskarkc51@gmail.com>
2025-03-11 23:20:01 +0545

    New

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,7 +3,7 @@ import { ReactTyped } from "react-typed";
 
 function Hero() {
   return (
-    <div className='text-white'>
+    <div className='text-white mt-24'>
       <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center '>
         <p className='text-[#00df9a] font-bold p-2'>WELCOME TO MY WEBSITE</p> 
         <h1 className='md:text-7xl sm:text-6xl text-4xl'>I am Sanskar K.c</h1>
