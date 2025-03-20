c5e9b2f270101aa634e8c5cbacab773dc3106b52
SansKc <sanskarkc51@gmail.com>
2025-03-20 17:25:52 +0545

    New Pic Changes

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -6,7 +6,8 @@ function Carousel() {
   const [activeItem, setActiveItem] = useState<number>(3);
 
   return (
-    <div className="flex items-center justify-center h-[800px] w-full bg-black">
+    <div className="flex flex-col items-center justify-center h-[800px] w-full bg-black gap-10">
+      <h1 className="relative text-white text-4xl font-bold ">My Circle</h1>
       <div className="w-[1200px] max-w-full">
         <ul className="flex group h-[500px] gap-2 justify-center">
           {persons.map((person, index) => (
@@ -23,7 +24,7 @@ function Carousel() {
               <div className="relative h-full w-full">
                 {/* Light Blue Gradient Overlay */}
                 <div className="absolute inset-0 bg-gradient-to-b from-purple-500/10 to-purple-900/40 z-10"></div>
-                
+
                 {/* Image */}
                 <img
                   className="h-full w-full object-cover grayscale"
@@ -35,7 +36,9 @@ function Carousel() {
               {/* Gradient Overlay for Name and Title */}
               {activeItem === index && (
                 <div className="absolute bottom-0 left-0 right-0 p-4 bg-gradient-to-t from-black/80 to-transparent z-20">
-                  <h2 className="text-2xl font-bold text-white">{person.name}</h2>
+                  <h2 className="text-2xl font-bold text-white">
+                    {person.name}
+                  </h2>
                   <p className="text-base text-white/80">{person.title}</p>
                 </div>
               )}
--- src/components/Carousel/persons.js
+++ src/components/Carousel/persons.js
@@ -1,46 +1,46 @@
-import img1 from '../../assets/img1.jpg'
-import img2 from '../../assets/Mine.jpg'
+import img2 from "../../assets/Mine.jpg";
 
 export const persons = [
-    {
-      img:  "https://scontent.fktm3-1.fna.fbcdn.net/v/t1.6435-9/125186806_1984593751678244_1128679149094966127_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=oO-zVzO8t0kQ7kNvgGK3FFU&_nc_oc=AdlVPRLQZ7sEfH7s48PL75z5_oYY9zDVEQ67kXA1hWPz1o2UCRHDHGIZ9PHSrC9aqL0bEUAMK5Xmj5DuhZ_Zh58B&_nc_zt=23&_nc_ht=scontent.fktm3-1.fna&_nc_gid=yMXuyBXBMcApyIhcc0d6CA&oh=00_AYFCm8G8QuKVyXdqjhLWF2FX7IC7ZgB-E93zZ_V_ioj0mA&oe=68036FFA",
-      title:
-        "chief financial officer and executive vice president for global finance, aon plc",
-      name: "Swapnil Koirala",
-    },
-    {
-      img: img2,
-      title: "revenue and financial management, stripe",
-      name: "Sanskar KC",
-    },
-    {
-      img: "https://scontent.fktm3-1.fna.fbcdn.net/v/t39.30808-6/481176811_2929463660570429_9011299074582847155_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=N-5P7bCSB-8Q7kNvgGyIcw3&_nc_oc=AdlyM_TxTpyeNbxI2tyNmus--NHe9O3Bjko2pD5Wlwl3m73laeHCtpfnj0PxjyBa1C7h9hdkH31ByCl_LhcWwGQP&_nc_zt=23&_nc_ht=scontent.fktm3-1.fna&_nc_gid=vBXnzDnH4g1TPn-xO4pxqQ&oh=00_AYFriQmdJ7Ohgtvktlm5k5JPoKZuIXwjYza30a04VjPXaQ&oe=67E1C96A",
-      title: "head of product marketing, stripe",
-      name: "Rajottam Pradhan",
-    },
-    {
-      img: "https://avatars.worldcubeassociation.org/uploads/user/avatar/2018SHRE06/1574348644.jpg",
-      title: "head of global sales, stripe",
-      name: "Rojan Shrestha",
-    },
-    {
-      img: "https://scontent.fktm3-1.fna.fbcdn.net/v/t39.30808-6/473830931_4211411479104919_5973137996819678271_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=833d8c&_nc_ohc=Xfew4bgzQRIQ7kNvgEUbN9_&_nc_oc=AdkvgXtNtHZva4_LI55NosvdSSwShIOBak4JtctZNSHxdbMu3X7SNV7FCtg_0wcR5OavdPT57X65OQMbTQP70tis&_nc_zt=23&_nc_ht=scontent.fktm3-1.fna&_nc_gid=674et3sMeylENwQjgRq1aQ&oh=00_AYGQ3_Quavt-qkVPlwHDLWMyeS6JXm2qLMZ7OYVFMTfv1g&oe=67E1C791",
-      title: "founder, pulley",
-      name: "Utkrista Parajuli",
-    },
-    {
-      img: "https://unsplash.com/photos/TMZuhaL2kHo/download?force=true&w=590&h=640",
-      title: "ceo and cofounder, crowdai",
-      name: "Devaki Raj",
-    },
-    {
-      img: "https://unsplash.com/photos/XO25cX2_0iE/download?force=true&w=590&h=640",
-      title: "co-head of payments, wix",
-      name: "Amit Sagiv",
-    },
-    {
-      img: "https://scontent.fktm3-1.fna.fbcdn.net/v/t39.30808-6/395501023_2733201886817718_697706371338505936_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=RjkYiQMtszgQ7kNvgFnI2Rs&_nc_oc=Adk0y51l72qsXMTwd9tzc-PnSI68vRFSLpVUbC02vduK2et0zPZyDXMd1N3fwqMVGv1o6wif6BB1Q2QBCvJRpigX&_nc_zt=23&_nc_ht=scontent.fktm3-1.fna&_nc_gid=DfzdOVsKz1U1Cl4kbJpgqQ&oh=00_AYF1QSSprxklaTPF6sfZhifLcUS5epn5jWJIF5PLjM9haA&oe=67E19869",
-      title: "general partner, andreessen horowitz",
-      name: "Sandesh Koirala",
-    },
-  ];
+  {
+    img: "https://scontent.fktm3-1.fna.fbcdn.net/v/t1.6435-9/125186806_1984593751678244_1128679149094966127_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=oO-zVzO8t0kQ7kNvgGK3FFU&_nc_oc=AdlVPRLQZ7sEfH7s48PL75z5_oYY9zDVEQ67kXA1hWPz1o2UCRHDHGIZ9PHSrC9aqL0bEUAMK5Xmj5DuhZ_Zh58B&_nc_zt=23&_nc_ht=scontent.fktm3-1.fna&_nc_gid=yMXuyBXBMcApyIhcc0d6CA&oh=00_AYFCm8G8QuKVyXdqjhLWF2FX7IC7ZgB-E93zZ_V_ioj0mA&oe=68036FFA",
+    title:
+      "chief financial officer and executive vice president for global finance, aon plc",
+    name: "Swapnil Koirala",
+  },
+  {
+    img: "https://scontent.fktm3-1.fna.fbcdn.net/v/t39.30808-6/481176811_2929463660570429_9011299074582847155_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=N-5P7bCSB-8Q7kNvgGyIcw3&_nc_oc=AdlyM_TxTpyeNbxI2tyNmus--NHe9O3Bjko2pD5Wlwl3m73laeHCtpfnj0PxjyBa1C7h9hdkH31ByCl_LhcWwGQP&_nc_zt=23&_nc_ht=scontent.fktm3-1.fna&_nc_gid=vBXnzDnH4g1TPn-xO4pxqQ&oh=00_AYFriQmdJ7Ohgtvktlm5k5JPoKZuIXwjYza30a04VjPXaQ&oe=67E1C96A",
+    title: "head of product marketing, stripe",
+    name: "Rajottam Pradhan",
+  },
+  {
+    img: "https://avatars.worldcubeassociation.org/uploads/user/avatar/2018SHRE06/1574348644.jpg",
+    title: "head of global sales, stripe",
+    name: "Rojan Shrestha",
+  },
+  {
+    img: img2,
+    title: "revenue and financial management, stripe",
+    name: "Sanskar KC",
+  },
+  {
+    img: "https://scontent.fktm3-1.fna.fbcdn.net/v/t39.30808-6/395501023_2733201886817718_697706371338505936_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=RjkYiQMtszgQ7kNvgFnI2Rs&_nc_oc=Adk0y51l72qsXMTwd9tzc-PnSI68vRFSLpVUbC02vduK2et0zPZyDXMd1N3fwqMVGv1o6wif6BB1Q2QBCvJRpigX&_nc_zt=23&_nc_ht=scontent.fktm3-1.fna&_nc_gid=DfzdOVsKz1U1Cl4kbJpgqQ&oh=00_AYF1QSSprxklaTPF6sfZhifLcUS5epn5jWJIF5PLjM9haA&oe=67E19869",
+    title: "general partner, andreessen horowitz",
+    name: "Sandesh Koirala",
+  },
+  {
+    img: "https://scontent.fktm3-1.fna.fbcdn.net/v/t39.30808-6/475824967_1674127999869551_5685212441920564111_n.jpg?stp=dst-jpg_s1080x2048_tt6&_nc_cat=110&ccb=1-7&_nc_sid=bdeb5f&_nc_ohc=ey5FyTYnlqoQ7kNvgEv7beH&_nc_oc=AdkwIGau9HuYNKOTaprrk75Kv6pUhUB9fwcTHC13kaGZfd574COBOAMeH8tyYD1C9f6vQMANW5Urh_1fhCyIxkpm&_nc_zt=23&_nc_ht=scontent.fktm3-1.fna&_nc_gid=PGZabIGitBkWdtE1FrtfLg&oh=00_AYFwR_-e7jacmLStd7Hb6Gf3yvzkaRNMu4fyZL1ogU82Zw&oe=67E1C89E",
+    title: "ceo and cofounder, crowdai",
+    name: "Prasan KC",
+  },
+  {
+    img: "https://scontent.fktm3-1.fna.fbcdn.net/v/t39.30808-6/475637543_3463938827248425_4625062636655487246_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=XudjPDplMl0Q7kNvgHibM6m&_nc_oc=AdkwchX9Ip12VBVHUfXiEKKM5bC4HIhYkM7iexnXkh61D2dS1X76fmDtMNYUv8enH2fBmcirs8Aecg070mnNoout&_nc_zt=23&_nc_ht=scontent.fktm3-1.fna&_nc_gid=mV8g9w7ihK1SbjFd0JRf6g&oh=00_AYGH7xevgwdbr84LosAzMPv0cXLCuSzvIEEjjPiEInArmQ&oe=67E1BE88",
+    title: "co-head of payments, wix",
+    name: "Grish Katwal",
+  },
+  {
+   
+    img: "https://scontent.fktm3-1.fna.fbcdn.net/v/t39.30808-6/473830931_4211411479104919_5973137996819678271_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=833d8c&_nc_ohc=Xfew4bgzQRIQ7kNvgEUbN9_&_nc_oc=AdkvgXtNtHZva4_LI55NosvdSSwShIOBak4JtctZNSHxdbMu3X7SNV7FCtg_0wcR5OavdPT57X65OQMbTQP70tis&_nc_zt=23&_nc_ht=scontent.fktm3-1.fna&_nc_gid=674et3sMeylENwQjgRq1aQ&oh=00_AYGQ3_Quavt-qkVPlwHDLWMyeS6JXm2qLMZ7OYVFMTfv1g&oe=67E1C791",
+    title: "founder, pulley",
+    name: "Utkrista Parajuli",
+  },
+];
